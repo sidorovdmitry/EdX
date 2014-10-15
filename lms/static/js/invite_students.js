e152c4b0225e8d6c1494e770649003613e9c9833
Aldiantoro Nugroho <aldi@labster.com>
2014-10-15 14:46:59 +0700

    ajax post to invite students

--- lms/static/js/invite_students.js
+++ lms/static/js/invite_students.js
@@ -1,4 +1,14 @@
 $(function() {
+    function postJSON(url, data, callback) {
+        $.ajax({type:'POST',
+            url: url,
+            dataType: 'json',
+            data: data,
+            success: callback,
+            headers : {'X-CSRFToken':$.cookie('csrftoken')}
+        });
+    }
+
     $(".invite-students").click(function(event) {
         $("#invite-students-course-name").text($(event.target).data("course-name"));
         $("#invite-students-course-location").val($(event.target).data("course-location"));
@@ -11,6 +21,16 @@ $(function() {
 
     $("#invite-students-form").submit(function(event) {
         var el = $(event.currentTarget);
+
+        var course_location = $("#invite-students-course-location").val();
+        var url = '/labster/duplicate-course/';
+        var data = {
+            source: course_location
+        };
+        postJSON(url, data, function(response) {
+            console.log(response);
+        });
+
         el.find("input[type=email]").val("");
 
         $("#invite-students-form").hide();
