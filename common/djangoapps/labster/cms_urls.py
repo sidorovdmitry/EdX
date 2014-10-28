b1151a47a25f1821144881bdfd9ca6aec5571465
Aldiantoro Nugroho <aldi@labster.com>
2014-10-28 15:10:11 +0700

    view to update lab's quiz block

--- common/djangoapps/labster/cms_urls.py
+++ common/djangoapps/labster/cms_urls.py
@@ -5,6 +5,7 @@ urlpatterns = patterns('labster.cms.views',  # nopep8
     url('^duplicate-lab/$', 'duplicate_lab', name='labster_duplicate_lab'),
     url('^duplicate-course/$', 'duplicate_course_view', name='labster_duplicate_course'),
     url('^manage-lab/$', 'manage_lab_view', name='labster_manage_lab'),
+    url('^update-course-block/(?P<lab_id>\d+)/$', 'update_quiz_block_view', name='labster_update_quiz_block'),
 )
 
 urlpatterns += patterns('',  # nopep8
