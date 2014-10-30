ee7bf21a8135ea1df750c27a5f651268e7c99aa5
aslam <aslamhadi@gmail.com>
2014-10-30 13:18:11 +0700

    Add directive numeric to allow only number in element

--- lms/static/labster/js/backoffice.angular.js
+++ lms/static/labster/js/backoffice.angular.js
@@ -126,4 +126,21 @@ angular.module('LabsterBackOffice', ['ngRoute'])
         });
       }
     }
+  })
+
+  .directive('numeric', function() {
+    return {
+      require: 'ngModel',
+      link: function (scope, element, attr, ngModelCtrl) {
+        function fromUser(text) {
+          var transformedInput = text.replace(/[^0-9]/g, '');
+          if(transformedInput !== text) {
+              ngModelCtrl.$setViewValue(transformedInput);
+              ngModelCtrl.$render();
+          }
+          return transformedInput;  // or return Number(transformedInput)
+        }
+        ngModelCtrl.$parsers.push(fromUser);
+      }
+    };
   });
