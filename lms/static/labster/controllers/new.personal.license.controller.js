8d88d3a58c52b02f240d422748b812b70b0e4a5a
aslam <aslamhadi@gmail.com>
2014-11-07 15:36:46 +0800

    List all eu countries

--- lms/static/labster/controllers/new.personal.license.controller.js
+++ lms/static/labster/controllers/new.personal.license.controller.js
@@ -10,6 +10,38 @@ angular.module('LabsterBackOffice')
     $scope.checkoutButton = "Checkout";
     $scope.is_personal = true;
 
+    $scope.eu_countries = [
+      {id:15, name:'Austria'},
+      {id:22, name:'Belgium'},
+      {id:35, name:'Bulgaria'},
+
+      {id:56, name:'Croatia'},
+      {id:59, name:'Cyprus'},
+      {id:60, name:'Czech Republic'},
+      {id:61, name:'Denmark'},
+      {id:70, name:'Estonia'},
+      {id:75, name:'Finland'},
+      {id:76, name:'France'},
+      {id:83, name:'Germany'},
+      {id:86, name:'Greece'},
+      {id:101, name:'Hungary'},
+      {id:107, name:'Ireland'},
+      {id:110, name:'Italy'},
+      {id:123, name:'Latvia'},
+      {id:129, name:'Lithuania'},
+      {id:130, name:'Luxembourg'},
+      {id:138, name:'Malta'},
+      {id:157, name:'Netherlands'},
+      {id:177, name:'Poland'},
+      {id:178, name:'Portugal'},
+      {id:182, name:'Romania'},
+      {id:202, name:'Slovakia'},
+      {id:203, name:'Slovenia'},
+      {id:208, name:'Spain'},
+      {id:214, name:'Sweden'},
+      {id:234, name:'United Kingdom'},
+    ];
+
     var group_type = $routeParams.group_type;
 
     $scope.group_type_name = function () {
