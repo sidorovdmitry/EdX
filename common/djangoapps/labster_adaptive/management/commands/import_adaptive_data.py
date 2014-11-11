057ae36a6ef551bb81c5787cd04588fbe4a2ac42
Aldiantoro Nugroho <aldi@labster.com>
2014-11-11 15:34:01 +0800

    import script for adaptive problems

--- common/djangoapps/labster_adaptive/management/commands/import_adaptive_data.py
+++ common/djangoapps/labster_adaptive/management/commands/import_adaptive_data.py
@@ -3,7 +3,7 @@ import csv
 from django.core.management.base import BaseCommand
 from django.utils import timezone
 
-from labster_adaptive.models import Scale, Station, Problem, Answer
+from labster_adaptive.models import Scale, Category, Problem, Answer
 
 
 """
@@ -14,6 +14,64 @@ https://s3-us-west-2.amazonaws.com/labster/adaptive/item_bank.csv
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        path = args[0]
+
+        # Order,ID,ItemID,Question,Correct Answer,Possible Answer 1,Possible Answer 2,Possible Answer 3,Possible Answer 4,Possible Answer 5,Image,Categories,Category
+        with open(path, 'rb') as csv_file:
+            reader = csv.reader(csv_file)
+            for row in list(reader)[1:]:
+                order = row[0].strip()
+                _ = row[1] # id not used
+                item_number = row[2].strip()
+                question = row[3].strip()
+                correct_answer = row[4].strip()
+                possible_answer_1 = row[5].strip()
+                possible_answer_2 = row[6].strip()
+                possible_answer_3 = row[7].strip()
+                possible_answer_4 = row[8].strip()
+                possible_answer_5 = row[9].strip()
+                image_url = row[10].strip()
+                _ = row[11] # categories not used
+                category = row[12].strip()
+
+                try:
+                    problem = Problem.objects.get(item_number=item_number)
+                except:
+                    problem = Problem(item_number=item_number)
+
+                problem.item_number = item_number
+                problem.question = question
+                problem.image_url = image_url
+                problem.modified_at = timezone.now()
+                problem.order = order
+                problem.save()
+
+                Answer.objects.filter(problem=problem).update(is_active=False)
+
+                def create_answer(problem, answer, is_correct):
+                    try:
+                        answer = Answer.objects.create(problem=problem, answer=answer)
+                    except Answer.DoesNotExist:
+                        answer = Answer(problem=problem, answer=answer)
+
+                    answer.is_correct = is_correct
+                    answer.is_active = True
+                    answer.save()
+
+                if correct_answer.upper() != 'N/A':
+                    create_answer(problem, correct_answer, True)
+                create_answer(problem, possible_answer_1, False)
+                create_answer(problem, possible_answer_2, False)
+                create_answer(problem, possible_answer_3, False)
+                create_answer(problem, possible_answer_4, False)
+                create_answer(problem, possible_answer_5, False)
+
+                category, _ = Category.objects.get_or_create(name=category)
+                problem.categories.add(category)
+
+            self.stdout.write('done!\n')
+
+    def import_item_back(self):
         with open('item_bank.csv', 'rb') as csv_file:
             reader = csv.reader(csv_file)
             for each in list(reader)[1:]:
