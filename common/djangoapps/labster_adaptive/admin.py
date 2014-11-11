8ebcbcbddc9e0db7a6737049b290bfd6c9a05cf9
Aldiantoro Nugroho <aldi@labster.com>
2014-11-11 15:33:46 +0800

    add display for problem

--- common/djangoapps/labster_adaptive/admin.py
+++ common/djangoapps/labster_adaptive/admin.py
@@ -1,8 +1,19 @@
 from django.contrib import admin
 
-from .models import Problem, Answer, Scale, Station
+from .models import Problem, Answer, Scale, Category
+
+
+class ProblemAdmin(admin.ModelAdmin):
+    list_display = (
+        'item_number',
+        'order',
+        'question',
+        'image_url',
+    )
+    list_filter = ('categories',)
+
 
 admin.site.register(Scale)
-admin.site.register(Station)
-admin.site.register(Problem)
+admin.site.register(Category)
+admin.site.register(Problem, ProblemAdmin)
 admin.site.register(Answer)
