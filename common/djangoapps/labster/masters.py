a58937d607bed26ce931b82f6db8bb55a107b7c3
Aldiantoro Nugroho <aldi@labster.com>
2014-11-18 14:51:42 +0700

    use order of elements as order of data

--- common/djangoapps/labster/masters.py
+++ common/djangoapps/labster/masters.py
@@ -1,12 +1,13 @@
 from lxml import etree
 import requests
 
-from labster.models import QuizBlock, Problem, Answer
+from labster.models import Lab, QuizBlock, Problem, Answer
 from labster.quiz_blocks import QUIZ_BLOCK_S3_PATH
 from labster.utils import get_hashed_text
 
 
 def create_quizblock_from_tree(lab, tree):
+    order = 1
     for child in tree.getchildren():
         if child.tag != 'QuizBlock':
             continue
@@ -15,12 +16,15 @@ def create_quizblock_from_tree(lab, tree):
 
         quiz_block, _ = QuizBlock.objects.get_or_create(lab=lab, element_id=element_id)
         quiz_block.is_active = True
+        quiz_block.order = order
         quiz_block.save()
 
         create_problem_from_tree(quiz_block, child)
+        order += 1
 
 
 def create_problem_from_tree(quiz_block, tree):
+    order = 1
     for child in tree.getchildren():
         if child.tag != 'Quiz':
             continue
@@ -34,6 +38,7 @@ def create_problem_from_tree(quiz_block, tree):
         problem.correct_message = child.attrib.get('CorrectMessage', '')
         problem.wrong_message = child.attrib.get('WrongMessage', '')
         problem.no_score = child.attrib.get('NoScore') == 'true'
+        problem.order = order
 
         try:
             max_attempts = int(child.attrib.get('MaxAttempts'))
@@ -45,9 +50,11 @@ def create_problem_from_tree(quiz_block, tree):
         problem.save()
 
         create_answer_from_tree(problem, child)
+        order += 1
 
 
 def create_answer_from_tree(problem, tree):
+    order = 1
     for options in tree.getchildren():
         if options.tag != 'Options':
             continue
@@ -68,7 +75,9 @@ def create_answer_from_tree(problem, tree):
             answer.is_correct = option.attrib.get('IsCorrectAnswer') == 'true'
             answer.is_active = True
             answer.text = text.encode('utf-8')
+            answer.order = order
             answer.save()
+            order += 1
 
 
 def fetch_quizblocks(lab):
@@ -89,3 +98,9 @@ def fetch_quizblocks(lab):
 
     qb_tree = etree.fromstring(response.content)
     create_quizblock_from_tree(lab, qb_tree)
+
+
+def fetch_labs_as_json():
+    labs = Lab.objects.order_by('name')
+    labs_json = [lab.to_json() for lab in labs]
+    return labs_json
