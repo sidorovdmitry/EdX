b4cb5b542f38858af5fd6ad4d5d798eefb0dc7fd
Aldiantoro Nugroho <aldi@labster.com>
2014-11-18 13:51:23 +0700

    refactor sync master lab

--- common/djangoapps/labster/management/commands/sync_master_lab.py
+++ common/djangoapps/labster/management/commands/sync_master_lab.py
@@ -8,6 +8,71 @@ from labster.quiz_blocks import QUIZ_BLOCK_S3_PATH
 from labster.utils import get_hashed_text
 
 
+def create_quizblock_from_tree(lab, tree):
+    for child in tree.getchildren():
+        if child.tag != 'QuizBlock':
+            continue
+
+        element_id = child.attrib.get('Id').strip()
+
+        quiz_block, _ = QuizBlock.objects.get_or_create(lab=lab, element_id=element_id)
+        quiz_block.is_active = True
+        quiz_block.save()
+
+        create_problem_from_tree(quiz_block, child)
+
+
+def create_problem_from_tree(quiz_block, tree):
+    for child in tree.getchildren():
+        if child.tag != 'Quiz':
+            continue
+
+        element_id = child.attrib.get('Id').strip()
+        problem, _ = Problem.objects.get_or_create(
+            quiz_block=quiz_block, element_id=element_id)
+
+        problem.is_active = True
+        problem.sentence = child.attrib.get('Sentence', '')
+        problem.correct_message = child.attrib.get('CorrectMessage', '')
+        problem.wrong_message = child.attrib.get('WrongMessage', '')
+        problem.no_score = child.attrib.get('NoScore') == 'true'
+
+        try:
+            max_attempts = int(child.attrib.get('MaxAttempts'))
+        except:
+            max_attempts = None
+
+        problem.max_attempts = max_attempts
+        problem.randomize_option_order = child.attrib.get('RandomizeOptionOrder') != 'false'
+        problem.save()
+
+        create_answer_from_tree(problem, child)
+
+
+def create_answer_from_tree(problem, tree):
+    for options in tree.getchildren():
+        if options.tag != 'Options':
+            continue
+
+        for option in options.getchildren():
+            if option.tag != 'Option':
+                continue
+
+            text = option.attrib.get('Sentence')
+            if not text:
+                continue
+
+            hashed_text = get_hashed_text(text)
+
+            answer, _ = Answer.objects.get_or_create(
+                problem=problem, hashed_text=hashed_text)
+
+            answer.is_correct = option.attrib.get('IsCorrectAnswer') == 'true'
+            answer.is_active = True
+            answer.text = text.encode('utf-8')
+            answer.save()
+
+
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
@@ -29,57 +94,4 @@ class Command(BaseCommand):
             assert response.status_code == 200, "missing quizblocks xml"
 
             qb_tree = etree.fromstring(response.content)
-            for qb_child in qb_tree.getchildren():
-                if qb_child.tag != 'QuizBlock':
-                    continue
-
-                element_id = qb_child.attrib.get('Id').strip()
-
-                quiz_block, _ = QuizBlock.objects.get_or_create(lab=lab, element_id=element_id)
-                quiz_block.is_active = True
-                quiz_block.save()
-
-                for problem_child in qb_child.getchildren():
-                    if qb_child.tag != 'Quiz':
-                        continue
-
-                    element_id = problem_child.attrib.get('Id').strip()
-                    problem, _ = Problem.objects.get_or_create(
-                        quiz_block=quiz_block, element_id=element_id)
-
-                    problem.is_active = True
-                    problem.sentence = problem_child.attrib.get('Sentence', '')
-                    problem.correct_message = problem_child.attrib.get('CorrectMessage', '')
-                    problem.wrong_message = problem_child.attrib.get('WrongMessage', '')
-                    problem.no_score = problem_child.attrib.get('NoScore') == 'true'
-
-                    try:
-                        max_attempts = int(problem_child.attrib.get('MaxAttempts'))
-                    except:
-                        max_attempts = None
-
-                    problem.max_attempts = max_attempts
-                    problem.randomize_option_order = problem_child.attrib.get('RandomizeOptionOrder') != 'false'
-                    problem.save()
-
-                    for options in problem_child.getchildren():
-                        if options.tag != 'Options':
-                            continue
-
-                        for option in options.getchildren():
-                            if option.tag != 'Option':
-                                continue
-
-                            text = option.attrib.get('Sentence')
-                            if not text:
-                                continue
-
-                            hashed_text = get_hashed_text(text)
-
-                            answer, _ = Answer.objects.get_or_create(
-                                problem=problem, hashed_text=hashed_text)
-
-                            answer.is_correct = option.attrib.get('IsCorrectAnswer') == 'true'
-                            answer.is_active = True
-                            answer.text = text.encode('utf-8')
-                            answer.save()
+            create_quizblock_from_tree(lab, qb_tree)
