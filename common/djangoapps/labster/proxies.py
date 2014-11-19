6d7cb21f110d0110f2a331963034c2fef2a7c86b
Aldiantoro Nugroho <aldi@labster.com>
2014-11-19 14:51:03 +0700

    function to get lab proxy in platform xml format

--- common/djangoapps/labster/proxies.py
+++ common/djangoapps/labster/proxies.py
@@ -4,7 +4,7 @@ from django.contrib.auth.models import User
 
 from xmodule.modulestore.django import modulestore
 
-from labster.masters import get_problem_as_platform_xml
+from labster.masters import get_problem_as_platform_xml, get_quiz_block_as_platform_xml
 from labster.models import LabProxy, Lab
 from labster.models import QuizBlock, Problem
 from labster.parsers.problem_parsers import QuizParser
@@ -33,7 +33,9 @@ def prepare_lab(lab, location):
         for problem in problems:
 
             # create component
-            component = create_component_from_problem(user, problem, unit.location)
+            component = create_component_from_problem(
+                user, lab_proxy, problem, unit.location)
+
             component = modulestore().get_item(component.location)
             modulestore().publish(component.location, user.id)
 
@@ -52,9 +54,11 @@ def create_unit_from_quiz_block(user, quiz_block, location):
     return unit
 
 
-def create_component_from_problem(user, problem, location):
+def create_component_from_problem(user, lab_proxy, problem, location):
     name = problem.element_id
     extra_post = {'boilerplate': "multiplechoice.yaml"}
+    quiz_block = problem.quiz_block
+
     component = create_xblock(
         user, 'problem', location.to_deprecated_string(), extra_post=extra_post)
 
@@ -72,4 +76,20 @@ def create_component_from_problem(user, problem, location):
         correct_answer=quiz_parser.correct_answer,
     )
 
+    get_or_create_problem_proxy_from_quiz(
+        lab_proxy, platform_xml, component.location, quiz_block.element_id)
+
     return component
+
+
+def get_lab_proxy_as_platform_xml(lab_proxy):
+    attrib = {'Id': str(lab_proxy.id)}
+    lab_proxy_el = etree.Element('Lab', attrib)
+    quizblocks_el = etree.SubElement(lab_proxy_el, 'QuizBlocks')
+
+    quiz_blocks = QuizBlock.objects.filter(is_active=True, lab=lab_proxy.lab)
+    for quiz_block in quiz_blocks:
+        quiz_block_el = get_quiz_block_as_platform_xml(quiz_block)
+        quizblocks_el.append(quiz_block_el)
+
+    return lab_proxy_el
