072a1f64c4e70832e42f6f836bf863ce472491b4
Aldiantoro Nugroho <aldi@labster.com>
2014-12-03 04:05:30 +0100

    views to log nutshell activities

--- common/djangoapps/labster/lms/views.py
+++ common/djangoapps/labster/lms/views.py
@@ -22,6 +22,7 @@ from opaque_keys.edx.locations import SlashSeparatedCourseKey
 
 from labster.models import LabProxy, UserSave, UserAttempt
 from labster.reports import get_attempts_and_answers
+from labster.tasks import send_play_lab, send_invite_students
 
 
 API_PREFIX = getattr(settings, 'LABSTER_UNITY_API_PREFIX', '')
@@ -282,9 +283,30 @@ class LabResult(DetailView):
         return context
 
 
+class NutshellPlayLab(View):
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        lab_id = kwargs.get('lab_id')
+
+        send_play_lab.delay(user.id, lab_id)
+        return HttpResponse(1)
+
+
+class NutshellInviteStudents(View):
+    def post(self, request, *args, **kwargs):
+        user = request.user
+        lab_id = kwargs.get('lab_id')
+
+        send_invite_students.delay(user.id, lab_id)
+        return HttpResponse(1)
+
+
+
 settings_xml = SettingsXml.as_view()
 server_xml = ServerXml.as_view()
 platform_xml = PlatformXml.as_view()
 start_new_lab = StartNewLab.as_view()
 continue_lab = ContinueLab.as_view()
 lab_result = login_required(LabResult.as_view())
+nutshell_play_lab = login_required(NutshellPlayLab.as_view())
+nutshell_invite_students = login_required(NutshellInviteStudents.as_view())
--- common/djangoapps/labster/urls.py
+++ common/djangoapps/labster/urls.py
@@ -6,5 +6,9 @@ urlpatterns = patterns('labster',  # nopep8
     url('^api/', include('labster.api_urls', namespace='labster-api')),
     url('^lab/{}/(?P<pk>\d+)/result/$'.format(settings.COURSE_ID_PATTERN),
         'lms.views.lab_result', name='labster_lab_result'),
+    url('^lab/{}/(?P<pk>\d+)/nutshell_play_lab/$'.format(settings.COURSE_ID_PATTERN),
+        'lms.views.nutshell_play_lab', name='labster_nutshell_play_lab'),
+    url('^lab/{}/(?P<pk>\d+)/nutshell_invite_students/$'.format(settings.COURSE_ID_PATTERN),
+        'lms.views.nutshell_invite_students', name='labster_nutshell_invite_students'),
     url('^demo-lab/$', 'lms.views.demo_lab', name='labster_demo_lab'),
 )
