27735593634f17cad15545a1dd36f4264f1c063c
aslam <aslamhadi@gmail.com>
2014-12-17 13:47:56 +0700

    Add lab information in student_license page

--- lms/static/labster/student_license/controllers/home.controller.js
+++ lms/static/labster/student_license/controllers/home.controller.js
@@ -1,5 +1,17 @@
 angular.module('LabsterStudentLicense')
 
-  .controller('HomeController', function ($scope) {
-    $scope.lab_id = window.lab_id;
+  .controller('HomeController', function ($scope, $http) {
+    $scope.agree_tos = false;
+
+    var url_product = window.backofficeUrls.product + window.lab_id + '/external_id/';
+    $http.get(url_product, {
+      headers: {
+        'Authorization': "Token " + window.requestUser.backoffice.token
+      }
+    })
+      .success(function (data, status, headers, config) {
+        $scope.lab_info = data;
+        $scope.lab_info.item_count = 1;
+        console.log(data);
+      });
   });
