74cc73a04240999bba7944955e8cf9688059b2d9
Aldiantoro Nugroho <aldi@labster.com>
2015-01-05 13:27:53 +0700

    function to export adaptive tests

--- common/djangoapps/labster/management/commands/export_answer_adaptive.py
+++ common/djangoapps/labster/management/commands/export_answer_adaptive.py
@@ -9,6 +9,53 @@ from labster.proxies import generate_lab_proxy_data
 class Command(BaseCommand):
 
     def handle(self, *args, **options):
+        self.export_adaptive_tests(*args, **options)
+
+    def export_adaptive_tests(self, *args, **options):
+        try:
+            lab_id = args[0]
+        except:
+            raise CommandError("Missing lab proxy's id arg")
+
+        try:
+            year, month, day = args[1].split('-')
+        except:
+            year = 2014
+            month = 12
+            day = None
+
+        try:
+            lab_proxy = LabProxy.objects.get(id=lab_id)
+        except LabProxy.DoesNotExist:
+            raise CommandError("Missing lab proxy")
+
+        filters = {}
+
+        if year:
+            filters['created_at__year'] = year
+            if month:
+                filters['created_at__month'] = month
+                if day:
+                    filters['created_at__day'] = day
+
+        quiz_blocks = [
+            'QuizblockSection1',
+            'QuizblockSection2',
+            'QuizblockSection3',
+            'QuizblockSection4',
+        ]
+        file_name = "adaptive_prepost_{}{}{}.csv".format(year, month, day)
+        generate_lab_proxy_data(lab_proxy, quiz_blocks=quiz_blocks, filters=filters,
+                                file_name=file_name)
+        # Cyto-1217-3-Post
+        quiz_ids = ["Cyto-1217-{}-Post".format(i) for i in range(1, 51)]
+        file_name = "adaptive_answers_{}{}{}.csv".format(year, month, day)
+        score_file_name = "adaptive_scores_{}{}{}.csv".format(year, month, day)
+        generate_lab_proxy_data(lab_proxy, quiz_ids=quiz_ids, filters=filters,
+                                file_name=file_name, process_score=True,
+                                score_file_name=score_file_name, active_only=True)
+
+    def export_adaptive_labs(self):
         try:
             lab_id = args[0]
         except:
