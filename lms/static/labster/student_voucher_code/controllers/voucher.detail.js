8fb6b5deefe232694a679ec1d5e26108627306ec
aslamhadi <aslamhadi@gmail.com>
2015-01-07 16:38:54 +0700

    Update voucher detail page

--- lms/static/labster/student_voucher_code/controllers/voucher.detail.js
+++ lms/static/labster/student_voucher_code/controllers/voucher.detail.js
@@ -2,6 +2,22 @@ angular.module('StudentVoucherCode')
 
   .controller('VoucherController', function ($scope, $location, $http, $routeParams) {
     $scope.voucher_code = $routeParams.voucher_code;
+    $scope.agree_tos = false;
+    $scope.btnPayment = "btn-labster-invoice labster-disabled-btn";
+
+    $scope.updateBtnPayment = function() {
+      if($scope.agree_tos) {
+        $scope.btnPayment = "btn-labster-invoice";
+      } else {
+        $scope.btnPayment = "btn-labster-invoice labster-disabled-btn";
+      }
+    };
+
+    $scope.goToPayment = function(){
+      if($scope.agree_tos) {
+        $location.path('/license/new/' + $scope.voucher_code);
+      }
+    };
 
     var url = window.backofficeUrls.voucher + $scope.voucher_code + "/";
     $http.get(url, {
@@ -9,6 +25,6 @@ angular.module('StudentVoucherCode')
         'Authorization': "Token " + window.requestUser.backoffice.token
       }})
       .success(function (data, status, headers, config) {
-        console.log(data);
+        $scope.voucher = data;
       });
   });
