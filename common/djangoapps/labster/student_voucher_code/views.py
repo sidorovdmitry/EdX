665411ccdd6c1212da1e884ff5f0879c666347e4
aslamhadi <aslamhadi@gmail.com>
2015-01-09 10:20:17 +0700

    Update view student_voucher_code to use LabsterUser

--- common/djangoapps/labster/student_voucher_code/views.py
+++ common/djangoapps/labster/student_voucher_code/views.py
@@ -7,18 +7,21 @@ from django.contrib.auth.decorators import login_required
 
 from student.models import UserProfile
 
+from labster.models import LabsterUser
+
 
 @login_required
 def home(request):
     template_name = 'labster/student_voucher_code.html'
     user_profile = UserProfile.objects.get(user=request.user)
+    labster_user = LabsterUser.objects.get(user=request.user)
     bo_user = create_user(request.user, user_profile.name, format='json')
 
     token = bo_user['token']
     backoffice = {
         'user_id': bo_user['id'],
         'user_country': user_profile.country,
-        'user_edu_level': user_profile.user_school_level
+        'user_edu_level': labster_user.user_school_level
     }
 
     backoffice_urls = get_backoffice_urls()
