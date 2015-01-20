91da3ef9b4ba28a310e3cc3243407113f4750c55
aslam <aslamhadi@gmail.com>
2015-01-20 16:00:34 +0800

    Update cancel order page

--- lms/static/labster/controllers/payment.cancel.controller.js
+++ lms/static/labster/controllers/payment.cancel.controller.js
@@ -11,30 +11,8 @@ angular.module('LabsterBackOffice')
         'Authorization': "Token " + window.requestUser.backoffice.token
       }})
     .success(function (data, status, headers, config) {
-      $scope.payment = data.payment;
-      $scope.licenses = data.licenses
-      console.log($scope.licenses);
-      // $scope.allPayments = data.payments;
-      // $scope.payment = $filter('filter')(data.payments, {id:$scope.paymentId})[0]
-      // $scope.payment.created_at = moment($scope.payment.created_at).format('ll')
-      //console.log($scope.allPayments);
+      $scope.allPayments = data.payments;
+      $scope.payment = $filter('filter')(data.payments, {id:$scope.paymentId})[0]
+      console.log($scope.allPayments);
     });
-
-    // var url_license = window.backofficeUrls.license;
-    // $http.get(url_license, {
-    //   headers: {
-    //     'Authorization': "Token " + window.requestUser.backoffice.token
-    //   }
-    // })
-
-    //   .success(function (data, status, headers, config) {
-    //     angular.forEach(data, function (item) {
-    //       item.end_date = moment(item.date_end_license).format('ll');
-    //       item.date_bought = moment(item.date_bought).format('ll');
-    //     });
-
-    //     $scope.licenses = data;
-    //     console.log("batman")
-    //     console.log(data);
-    //   });
   });
