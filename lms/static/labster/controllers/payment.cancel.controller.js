c70b31e563ab40869bdf13ab0b070f893d8cb528
aslam <aslamhadi@gmail.com>
2015-01-20 20:56:34 +0800

    Call api to cancel order

--- lms/static/labster/controllers/payment.cancel.controller.js
+++ lms/static/labster/controllers/payment.cancel.controller.js
@@ -1,7 +1,6 @@
 angular.module('LabsterBackOffice')
 
   .controller('PaymentCancelController', function ($scope, $filter, $routeParams, $location, $http) {
-    $scope.batman = "batman";
     $scope.paymentId = $routeParams.paymentId;
 
     var url = window.backofficeUrls.payment + $scope.paymentId + "/cancel_order/";
@@ -15,4 +14,16 @@ angular.module('LabsterBackOffice')
       $scope.payment = $filter('filter')(data.payments, {id:$scope.paymentId})[0]
       console.log($scope.allPayments);
     });
+
+    $scope.cancelOrder = function(){
+      var post_data = {pk: $scope.paymentId};
+      $http.post(url, post_data, {
+        headers: {
+          'Authorization': "Token " + window.requestUser.backoffice.token
+        }
+      }).success(function (data, status, headers, config) {
+        console.log("success");
+      }).error(function (data, status, headers, config) {
+      });
+    }
   });
