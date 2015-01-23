b4065156c566da4fed1a3d6c975711b7748094cf
Aldiantoro Nugroho <kriwil@gmail.com>
2015-01-23 22:02:53 +0800

    add display_name to LabKeyword

--- common/djangoapps/labster_search/models.py
+++ common/djangoapps/labster_search/models.py
@@ -9,6 +9,7 @@ class LabKeyword(models.Model):
 
     lab = models.ForeignKey(Lab)
     keyword = models.CharField(max_length=255, db_index=True)
+    display_name = models.CharField(max_length=255, blank=True, default="")
     rank = models.IntegerField(default=0)
     frequency = models.IntegerField(default=0, db_index=True)
 
