ba7207e0f75b9b3f0ec5b5657beb0fb83014c927
aslam <aslamhadi@gmail.com>
2015-01-23 17:42:01 +0800

    Get popular labs

--- lms/djangoapps/courseware/courses.py
+++ lms/djangoapps/courseware/courses.py
@@ -11,6 +11,7 @@ from django.utils import timezone
 from edxmako.shortcuts import render_to_string
 from xmodule.modulestore import ModuleStoreEnum
 from opaque_keys.edx.keys import CourseKey
+from opaque_keys.edx.locations import SlashSeparatedCourseKey
 from xmodule.modulestore.django import modulestore
 from xmodule.contentstore.content import StaticContent
 from xmodule.modulestore.exceptions import ItemNotFoundError
@@ -324,6 +325,21 @@ def get_courses(user, domain=None):
     courses = [c for c in courses if has_access(user, 'see_exists', c)]
 
     courses = sorted(courses, key=lambda course: course.number)
+
+    return courses
+
+
+def get_popular_courses_labster(courses_id, max=6):
+    '''
+    Returns a list of most popular courses
+    '''
+    courses = []
+
+    for course_id in courses_id[:max]:
+        course_key = SlashSeparatedCourseKey.from_deprecated_string(course_id)
+        course = modulestore().get_course(course_key, depth=1)
+        if course:
+            courses.append(course)
 
     return courses
 
