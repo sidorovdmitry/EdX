528e53efa78c88d14f901ae1ba13c30ee2241eb2
aslam <aslamhadi@gmail.com>
2015-01-28 13:42:52 +0800

    Improve payment system

--- lms/static/labster/js/backoffice.angular.directives.js
+++ lms/static/labster/js/backoffice.angular.directives.js
@@ -42,7 +42,7 @@ angular.module('LabsterBackOffice')
           });
         });
       },
-      template: '<a class="btn-labster-invoice"><i class=\'fa fa-credit-card\'></i> Credit Card</a>',
+      template: '<a class="btn-labster-regular width-25"><i class=\'fa fa-credit-card\'></i> Pay with Credit Card</a>',
       replace: true
     };
   })
