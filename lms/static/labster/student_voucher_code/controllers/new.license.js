bda22e8fdbabc55a083dd3e3a36efe921795d575
aslam <aslamhadi@gmail.com>
2015-02-05 17:58:12 +0800

    Enable voucher 0

--- lms/static/labster/student_voucher_code/controllers/new.license.js
+++ lms/static/labster/student_voucher_code/controllers/new.license.js
@@ -1,6 +1,6 @@
 angular.module('StudentVoucherCode')
 
-  .controller('NewLicenseController', function ($scope, $routeParams, $http, $timeout) {
+  .controller('NewLicenseController', function ($scope, $routeParams, $http, $timeout, ngDialog) {
     $scope.user = window.requestUser;
     var user_id = window.requestUser.backoffice.user.id;
     var url = window.backofficeUrls.payment + 'voucher_code/';
@@ -30,7 +30,12 @@ angular.module('StudentVoucherCode')
         $scope.voucher = data.voucher;
         $scope.limit_reached = data.limit_reached;
         // console.log(data);
-        checkStripe();
+        if (data.response_license_id != 0){
+          // voucher price is zero, enroll the student
+          enrollStudent(data.response_license_id);
+        } else {
+          checkStripe();
+        }
       });
 
     function checkStripe() {
@@ -40,5 +45,30 @@ angular.module('StudentVoucherCode')
           angular.element('#stripe-button').trigger('click');
         }, 100);
       }
+    };
+
+    function enrollStudent(license_id) {
+      ngDialog.open({
+          template: '<h2 class="align-center">Please wait. We are processing your payment.</h2>',
+          plain: true,
+          showClose: false
+      })
+
+      $http.post(
+        '/labster/enroll-student/',
+        {
+          'license_id': license_id,
+          'email': $scope.user.email
+        },
+        {
+            headers: {
+                'X-CSRFToken': window.csrfToken
+            }
+        })
+        .success(function(data, status, headers, config) {
+          ngDialog.close();
+          var url = '/student_voucher_code/#/invoice/' + $scope.payment.id + '/thank-you';
+          window.location.href = url;
+        });
     };
   });
