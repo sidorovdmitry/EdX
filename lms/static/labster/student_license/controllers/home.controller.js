0d011e4561b55b7c64e9e4ed4df5cf6f93990c59
aslam <aslamhadi@gmail.com>
2015-02-11 17:23:30 +0700

    Update home student license

--- lms/static/labster/student_license/controllers/home.controller.js
+++ lms/static/labster/student_license/controllers/home.controller.js
@@ -2,6 +2,8 @@ angular.module('LabsterStudentLicense')
 
   .controller('HomeController', function ($scope, $http) {
     $scope.agree_tos = false;
+    $scope.is_submitting = false;
+    $scope.btnPayment = "btn-labster-invoice labster-disabled-btn";
 
     var url_product = window.backofficeUrls.product + window.lab_id + '/external_id/';
     $http.get(url_product, {
@@ -18,7 +20,17 @@ angular.module('LabsterStudentLicense')
         console.log(data);
       });
 
-    $scope.buyLab = function () {
+    $scope.updateBtnPayment = function() {
+      if($scope.agree_tos) {
+        $scope.btnPayment = "btn-labster-invoice";
+      } else {
+        $scope.btnPayment = "btn-labster-invoice labster-disabled-btn";
+      }
+    };
 
+    $scope.buyLab = function () {
+      if ($scope.agree_tos == true) {
+        $scope.is_submitting = true;
+      }
     }
   });
