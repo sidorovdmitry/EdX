549119d1954027724c91f6b457276b431df47ac0
aslam <aslamhadi@gmail.com>
2015-02-16 13:22:02 +0700

    Increase trailer video size #26907560063236

---
+++ lms/static/js/labster/course_about.js
@@ -0,0 +1,8 @@
+$(document).ready(function () {
+  // set youtube iframe
+  var iframe_video = $('#video-modal .inner-wrapper iframe');
+  iframe_video.css({
+    'height': 500,
+    'width': 882
+  });
+});
