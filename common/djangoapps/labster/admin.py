1eb7189541ba8c7c5d27694ffab4f2d739369a3d
Aldiantoro Nugroho <kriwil@gmail.com>
2015-02-26 10:28:39 +0700

    fix search in LabsterCourseLicense and LabsterUser admin

--- common/djangoapps/labster/admin.py
+++ common/djangoapps/labster/admin.py
@@ -264,7 +264,7 @@ class UserAttemptAdmin(admin.ModelAdmin):
 
 class LabsterCourseLicenseAdmin(admin.ModelAdmin):
     list_display = ('user', 'user_id', 'course_id', 'license_id')
-    search_fields = ('user__id', 'course_id')
+    search_fields = ('user__id', 'user__email', 'user__username', 'course_id')
 
     def user_id(self, obj):
         return obj.user.id
