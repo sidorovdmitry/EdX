5f586dde713ac1e8803a7dbde9f72c1b0a072681
Aldiantoro Nugroho <kriwil@gmail.com>
2015-02-26 09:42:35 +0700

    form for the add teacher to license
     #27694635667925

--- common/djangoapps/labster_admin/views.py
+++ common/djangoapps/labster_admin/views.py
@@ -1,4 +1,6 @@
+from django.contrib import messages
 from django.contrib.auth.decorators import user_passes_test
+from django.core.urlresolvers import reverse
 from django.utils.decorators import method_decorator
 from django.views.generic import TemplateView, FormView
 
@@ -24,10 +26,21 @@ class AddTeacherToLicense(StaffMixin, FormView):
     template_name = "labster_admin/add_teacher_to_license.html"
     form_class = TeacherToLicenseForm
 
+    def get_success_url(self):
+        return reverse('labster_add_teacher_to_license')
+
     def get_context_data(self, **kwargs):
         context = super(AddTeacherToLicense, self).get_context_data(**kwargs)
         context['is_add_teacher_to_license'] = True
         return context
+
+    def form_valid(self, form):
+        user, course_ids = form.save()
+        for course_id in course_ids:
+            messages.success(
+                self.request,
+                "<strong>{}</strong> is added to <strong>{}</strong>".format(user, course_id))
+        return super(AddTeacherToLicense, self).form_valid(form)
 
 
 home = Home.as_view()
