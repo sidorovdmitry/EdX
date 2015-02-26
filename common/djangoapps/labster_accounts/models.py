dcad5631ee0f43d583f4b3aff0a7d1041b4c0944
Aldiantoro Nugroho <kriwil@gmail.com>
2015-02-26 14:03:59 +0700

    admin and function to get or create organization
     #25058448919281

--- common/djangoapps/labster_accounts/models.py
+++ common/djangoapps/labster_accounts/models.py
@@ -5,8 +5,9 @@ from django.utils import timezone
 
 class Organization(models.Model):
     name = models.CharField(max_length=255, db_index=True)
+    label = models.CharField(max_length=255, default='', blank=True)
 
     created_at = models.DateTimeField(default=timezone.now)
 
     def __unicode__(self):
-        return self.name
+        return self.label if self.label else self.name
