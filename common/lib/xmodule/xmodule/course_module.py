d8ee7c77c2b46f0bbd5315c8d87d87d8a957cb79
Aldiantoro Nugroho <kriwil@gmail.com>
2015-03-13 12:49:08 +0700

    move LabsterCourseFields to xmodule.course_module
    to prevent circular import

--- common/lib/xmodule/xmodule/course_module.py
+++ common/lib/xmodule/xmodule/course_module.py
@@ -21,8 +21,6 @@ from xblock.fields import Scope, List, String, Dict, Boolean, Integer, Float
 from .fields import Date
 from django.utils.timezone import UTC
 
-from labster.course_module import LabsterCourseFields
-
 log = logging.getLogger(__name__)
 
 # Make '_' a no-op so we can scrape strings
@@ -816,6 +814,52 @@ class CourseFields(object):
         help=_("Content module identifier (location) of entrance exam."),
         default=None,
         scope=Scope.settings,
+    )
+
+
+class LabsterCourseFields(object):
+
+    is_browsable = Boolean(
+        display_name=_("Course is Displayed in Front Page"),
+        help=_("Enter true or false. If true, the course is displayed. If false, the course is hidden."),
+        scope=Scope.settings,
+        default=False,
+    )
+    show_course_info = Boolean(
+        display_name=_("Course Info is Shown"),
+        help=_("Enter true or false. If true, the course is displayed. If false, the course info is hidden."),
+        scope=Scope.settings,
+        default=True,
+    )
+    main_wiki_page = String(
+        display_name=_("Main Wiki Page for the Course"),
+        help=_("Enter the page for the main page. If empty, it'll use default /wiki/course.id/"),
+        scope=Scope.settings,
+        default="",
+    )
+    labster_demo = Boolean(
+        display_name=_("Labster Demo Course"),
+        help=_("Enter true or false. If true, it will have all demo features."),
+        scope=Scope.settings,
+        default=False,
+    )
+    labster_license = Boolean(
+        display_name=_("Labster License Course"),
+        help=_("Enter true or false. If true, it will validate the student register."),
+        scope=Scope.settings,
+        default=False,
+    )
+    labster_verified = Boolean(
+        display_name=_("Labster Verified Lab"),
+        help=_("Enter true or false. If true, it will check for user's verified status before showing the lab."),
+        scope=Scope.settings,
+        default=False,
+    )
+    labtser_language = String(
+        display_name=_("Labster Lab Language"),
+        help=_("Enter the language code for the Lab. The default is en."),
+        scope=Scope.settings,
+        default="en",
     )
 
 
