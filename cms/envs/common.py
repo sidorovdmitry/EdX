15d6c1772ec677c5de45bc83ec32543301c1bc88
aslam <aslamhadi@gmail.com>
2015-03-16 16:50:34 +0700

    update common.py with url settings

--- cms/envs/common.py
+++ cms/envs/common.py
@@ -815,7 +815,8 @@ if FEATURES.get('LABSTER'):
     CORS_ORIGIN_ALLOW_ALL = True
     LABSTER_ENABLE_NUTSHELL = False
     LABSTER_ENABLE_SALESFORCE = False
-    LABSTER_BACKOFFICE_BASE_URL = "/labster/internal/"
+    LABSTER_UNITY_API_PREFIX = "https://www.labster.com"
+    LABSTER_BACKOFFICE_BASE_URL = "{}/labster/internal".format(LABSTER_UNITY_API_PREFIX)
     LABSTER_BACKOFFICE_JS_BASE_URL = LABSTER_BACKOFFICE_BASE_URL
 
     LABSTER_S3_BASE_URL = "https://labster.s3.amazonaws.com/"
