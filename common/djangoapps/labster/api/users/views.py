f14ce08d04c9cac11870935c9b77962b15bf8eb8
aslam <aslamhadi@gmail.com>
2015-03-20 16:46:56 +0700

    add ip number in registration. #29524056322964

--- common/djangoapps/labster/api/users/views.py
+++ common/djangoapps/labster/api/users/views.py
@@ -16,7 +16,7 @@ from labster.api.views import AuthMixin
 from labster.models import LabsterUser
 
 
-def get_user_as_custom_labster_user(user, password=None):
+def get_user_as_custom_labster_user(user, password=None, ip_number=None):
     user = User.objects.get(id=user.id)
     labster_user = user.labster_user
     profile, _ = UserProfile.objects.get_or_create(user=user)
@@ -34,6 +34,7 @@ def get_user_as_custom_labster_user(user, password=None):
         organization_name=labster_user.organization_name,
         user_school_level=labster_user.user_school_level,
         user_school_level_display=labster_user.get_user_school_level_display(),
+        ip_number = ip_number,
         date_of_birth=labster_user.date_of_birth,
         name=profile.name,
         password=password,
@@ -94,7 +95,8 @@ class UserView(AuthMixin, generics.RetrieveUpdateAPIView):
             raise Http404
 
         password = self.request.DATA.get('password')
-        return get_user_as_custom_labster_user(user, password)
+        ip_number = self.request.META.get('REMOTE_ADDR', None)
+        return get_user_as_custom_labster_user(user, password, ip_number)
 
     def get_queryset(self):
         return User.objects.all()
--- common/djangoapps/labster/users/admin.py
+++ common/djangoapps/labster/users/admin.py
@@ -121,7 +121,7 @@ make_inactive.short_description = "Set inactive"
 
 
 class LabsterUserAdmin(admin.ModelAdmin):
-    list_display = ('email', 'user_id', 'username', 'user_type_display', 'is_active')
+    list_display = ('email', 'user_id', 'username', 'user_type_display', 'is_active', 'ip_number')
     search_fields = ('user__email', 'user__username',)
     list_filter = ('user__is_active', 'user_type', 'is_new')
     raw_id_fields = ('user',)
