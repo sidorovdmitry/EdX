18d097aaf4aff82f198450df543aa0e8325df0b0
Aldiantoro Nugroho <kriwil@gmail.com>
2015-03-27 15:13:35 +0700

    use the better url if available
     #26907560063250

--- common/djangoapps/labster_frontend/demo_courses.py
+++ common/djangoapps/labster_frontend/demo_courses.py
@@ -1,3 +1,5 @@
+from django.core.urlresolvers import reverse
+
 from opaque_keys.edx.locations import SlashSeparatedCourseKey
 
 from labster.courses import get_demo_courses, get_lab_by_course_id
@@ -16,3 +18,25 @@ def populate_demo_courses():
             obj = DemoCourse(course_id=course_key)
         obj.lab = lab
         obj.save()
+
+
+def labsterify_courses(courses):
+    """
+    fetch labster demo course link if available
+    """
+
+    for course in courses:
+        try:
+            demo_course = DemoCourse.objects.get(course_id=course.id)
+        except DemoCourse.DoesNotExist:
+            slug = ''
+        else:
+            slug = demo_course.slug
+
+        if slug:
+            url = reverse('labster_demo_course', args=[demo_course.slug])
+        else:
+            url = reverse('about_course', args=[course.id.to_deprecated_string()])
+        course.labster_demo_course_url = url
+
+    return courses
