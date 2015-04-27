fb4df5785522d9faf0df5340e498c6b5d4f867a9
aslamhadi <aslamhadi@gmail.com>
2015-04-27 17:36:09 +0700

    background task for payment product

--- common/djangoapps/labster_admin/csv_views.py
+++ common/djangoapps/labster_admin/csv_views.py
@@ -107,15 +107,14 @@ class UploadCsvPaymentProduct(StaffMixin, FormView):
     def get_success_url(self):
         return reverse('labster-backoffice:payment:upload-csv-payment-product')
 
-    def get_context_data(self, **kwargs):
-        context = super(UploadCsvPaymentProduct, self).get_context_data(**kwargs)
-        return context
+    def get_form_kwargs( self ):
+        kwargs = super( UploadCsvPaymentProduct, self ).get_form_kwargs()
+        kwargs['user'] = self.request.user
+        return kwargs
 
     def form_valid(self, form):
         form.save()
-
-        messages.success(self.request, "We have imported all of the payment products")
-
+        messages.success(self.request, QUEUED_MESSAGE)
         return super(UploadCsvPaymentProduct, self).form_valid(form)
 
 
