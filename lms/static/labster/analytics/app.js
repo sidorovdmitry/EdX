207f78c2c2d50d6f5973a8336c83da1e8e28fbb0
Aldiantoro Nugroho <kriwil@gmail.com>
2015-07-03 06:30:49 +0700

    fix career page can't load detail job
    the angular is messing with the hash in the url that causes
    recruiterbox fails to read it properly

--- lms/static/labster/analytics/app.js
+++ lms/static/labster/analytics/app.js
@@ -1,1 +1,4 @@
-angular.module('LabsterAnalytics', ['angulartics', 'angulartics.google.tagmanager', 'angulartics.google.analytics']);
+angular.module('LabsterAnalytics', ['angulartics', 'angulartics.google.tagmanager', 'angulartics.google.analytics'])
+    .config(['$locationProvider', function($locationProvider) {
+        $locationProvider.html5Mode({enabled: true, requireBase: false});
+    }]);
