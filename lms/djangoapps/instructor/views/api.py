3178b902ff4fd3337b4d0b914a5130aac51f7ebe
den <den99f@me.com>
2015-10-15 15:41:27 +0300

    Change separator in instructor_tab download_data

--- lms/djangoapps/instructor/views/api.py
+++ lms/djangoapps/instructor/views/api.py
@@ -2098,7 +2098,7 @@ def student_quiz_detail_csv(request, course_id):
     response = HttpResponse(content_type='text/csv')
     response['Content-Disposition'] = 'attachment; filename="{}.csv"'.format(filename)
 
-    writer = csv.writer(response, quoting=csv.QUOTE_ALL)
+    writer = csv.writer(response, dialect='excel-tab', quoting=csv.QUOTE_ALL)
     attempts_type = request.GET.get('attempts_type')
     rows = export_answers(lab_proxy, attempts_type)
 
