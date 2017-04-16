41b24e5c0d57c5ba0e2727aea4722712b91a3355
Vladas Tamoshaitis <amd.vladas@gmail.com>
2017-04-17 02:02:17 +0300

    revert changes to urls.py

--- lms/djangoapps/labster_course_license/urls.py
+++ lms/djangoapps/labster_course_license/urls.py
@@ -2,11 +2,10 @@
 URLs for the Labster Course License Feature.
 """
 from django.conf.urls import patterns, url
-from django.views.decorators.csrf import csrf_exempt
-from labster_course_license.views import LicensedSimulationsUpdateView
 
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     url(
         r'^labster_license/?$',
         'labster_course_license.views.license_handler', name='labster_license_handler'
